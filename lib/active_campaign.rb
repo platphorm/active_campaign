caaf2dce1a12f9fcbcd099d5f1dcc8557ec6089a
anil_platphorm <anil@platphormcorp.com>
2015-06-02 14:26:15 -0700

    Fixing class methods

--- lib/active_campaign.rb
+++ lib/active_campaign.rb
@@ -9,33 +9,34 @@ require 'ostruct'
 module ActiveCampaign
   extend Configuration
 
-  module_function
-
   # API client based on configured options {Configurable}
   #
   # @return [ActiveCampaign::Client] API wrapper
-  def client
-    unless defined?(@client) && @client.same_options?(configuration)
-      @client = ActiveCampaign::Client.new(configuration)
+
+  class << self
+    def client
+      unless defined?(@client) && @client.same_options?(configuration)
+        @client = ActiveCampaign::Client.new(configuration)
+      end
+
+      @client
     end
 
-    @client
-  end
+    # @private
+    def respond_to_missing?(method_name, include_private = false)
+      client.respond_to?(method_name, include_private)
+    end if RUBY_VERSION >= '1.9'
 
-  # @private
-  def respond_to_missing?(method_name, include_private = false)
-    client.respond_to?(method_name, include_private)
-  end if RUBY_VERSION >= '1.9'
+    # @private
+    def respond_to?(method_name, include_private = false)
+      client.respond_to?(method_name, include_private) || super
+    end if RUBY_VERSION < '1.9'
 
-  # @private
-  def respond_to?(method_name, include_private = false)
-    client.respond_to?(method_name, include_private) || super
-  end if RUBY_VERSION < '1.9'
+    private
 
-  private
-
-  def method_missing(method_name, *args, &block)
-    return super unless client.respond_to?(method_name)
-    client.send(method_name, *args, &block)
+    def method_missing(method_name, *args, &block)
+      return super unless client.respond_to?(method_name)
+      client.send(method_name, *args, &block)
+    end
   end
 end
