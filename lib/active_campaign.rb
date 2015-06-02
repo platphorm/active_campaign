78f7fdb3d9b96501fee564f072639bc66891b29c
Anil Rhemtulla <anil@platphormcorp.com>
2015-06-02 14:12:39 -0700

    Adding missing references

--- lib/active_campaign.rb
+++ lib/active_campaign.rb
@@ -1,8 +1,10 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'active_support/core_ext/module/delegation'
+require 'active_campaign/version'
 require 'active_campaign/client'
 require 'active_campaign/configuration'
+require 'ostruct'
 
 module ActiveCampaign
   extend Configuration
